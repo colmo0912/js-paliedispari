2f9d28103c5164591afd27a01d6ae3223fa2c1ea
colmo0912 <colmo0912@gmail.com>
2021-04-14 18:28:34 +0200

    aggiungo if/else per segnare chi vince

--- dispari/script.js
+++ dispari/script.js
@@ -8,22 +8,37 @@ var sceltaNumeroUtente =parseInt(prompt("scegli un numero da 1 a 5"));
 
 //verifico se è pari o dispari tramite un if e la %
 var numeroPari;
-var numeroDispari;
-
+// var sceltaNumeroUtentePari;
+// var sceltaNumeroUtenteDispari;
 
 if(sceltaNumeroUtente % 2 === 0){
-    numeroPari=true
-    console.log("il numero da te scelto è pari" )
+    // sceltaNumeroUtentePari
+    console.log("il numero da te scelto " + sceltaNumeroUtente + " è pari" )
 }else {
-    numeroDispari=true
-    console.log("il numero da te scelto è dispari" )
+    // sceltaNumeroUtenteDispari
+    console.log("il numero da te scelto " + sceltaNumeroUtente + " è dispari" )
 }
 
+//genero un numero random per il pc da 1 a 5
 var numeroPC = Math.floor(Math.random() * 6); 
 console.log(numeroPC)
 
+//faccio la somma dei due numeri
+
 var somma = sceltaNumeroUtente + numeroPC
 console.log(somma)
+
+//faccio un if/else per controllare se la somma è pari o dispari
+if (sceltaNumeroUtente % 2 === 0 && somma % 2 === 0){
+
+    console.log("utente vince")
+}else {
+
+    console.log("PC vince")
+}
+
+
+
 
 
 
