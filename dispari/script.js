56cfaa5dcfa449ba3b50b98828d1c5b464164c12
colmo0912 <colmo0912@gmail.com>
2021-04-14 19:36:10 +0200

    aggiungo funzione che controlla pari e dispari

--- dispari/script.js
+++ dispari/script.js
@@ -19,24 +19,43 @@ if(sceltaNumeroUtente % 2 === 0){
     console.log("il numero da te scelto " + sceltaNumeroUtente + " è dispari" )
 }
 
+
 //genero un numero random per il pc da 1 a 5
 var numeroPC = Math.floor(Math.random() * 6); 
-console.log(numeroPC)
+console.log("il numero scelto dal PC è " + numeroPC)
 
 //faccio la somma dei due numeri
-
 var somma = sceltaNumeroUtente + numeroPC
 console.log(somma)
 
 //faccio un if/else per controllare se la somma è pari o dispari
-if (sceltaNumeroUtente % 2 === 0 && somma % 2 === 0){
+// if (sceltaNumeroUtente % 2 === 0 && somma % 2 === 0){
+
+//     console.log("utente vince")
+// }else {
+
+//     console.log("PC vince")
+// }
+
+
+function controlloPoD(pariodispari){
+
+    if (sceltaNumeroUtente % 2 === 0 && somma % 2 === 0){
+    return true
+    }else {
+
+        return false
+    }
+}
+
+var risultato = controlloPoD(somma)
+if (risultato){
 
     console.log("utente vince")
 }else {
 
     console.log("PC vince")
 }
-
 
 
 
